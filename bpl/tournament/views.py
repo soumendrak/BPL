9ddc22f9c0816c200a74b07e789fa7dcc898def6
Soumendra kumar sahoo <soumendra.sahoo@freshworks.com>
2023-10-31 10:15:46 +0530

    1st webpage created

--- bpl/tournament/views.py
+++ bpl/tournament/views.py
@@ -1,3 +1,13 @@
-# from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from .models import Tournament
+
 
 # Create your views here.
+@login_required
+@require_http_methods(["GET"])
+def tournaments_list(request):
+    tournaments = Tournament.objects.all()
+    return render(request, "tournaments/tournaments_dashboard.html", {"tournaments": tournaments})
--- config/urls.py
+++ config/urls.py
@@ -8,8 +8,11 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
+from bpl.tournament import views as tournamentViews
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", tournamentViews.tournaments_list, name="home"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
